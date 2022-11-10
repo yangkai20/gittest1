110e75c267ab037a9c16e05787556f951ca6f7e3
yangkai <yangkai@atguigu.com>
2022-11-10 15:41:40 +0800

    second commit

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("uamh");
         System.out.println("aaaa");
+        System.out.println("bbbbb");
     }
 }
